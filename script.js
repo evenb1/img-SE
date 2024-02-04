1b90e723f0785e3b9a1cb006ca603718fbb4d4f2
Even <even.russom@strathmore.edu>
2024-02-04 14:59:41 +0300

    final commit

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const searchForm = document.getElementsByClassName("search-form");
-const searchBox = document.getElementsByClassName("search-box");
-const searchResult = document.getElementsByClassName("search-result");
-const searchMore = document.getElementsByClassName("show-more");
+const searchForm = document.getElementById("search");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMore = document.getElementById("showMore");
 const accessKey = "0ebS3cbRmq1Nmq-JZQ1XyDOqgKHxuJUDLc6961N9sKw";
 
-let keyword="";
+let keyword = "";
 let page = 1;
 
-async function searchImages(){
+async function searchImages() {
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${acessKey}`;
-    
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=20`;
+
     const response = await fetch(url);
     const data = await response.json();
-
-const results = data.results;
-    results.map((result) =>{
+    if (page === 1) {
+        searchResult.innerHTML = "";
+    }
+    const results = data.results;
+    results.forEach((result) => {
         const image = document.createElement("img");
         image.src = result.urls.small;
         const imageLink = document.createElement("a");
@@ -23,11 +25,18 @@ const results = data.results;
         imageLink.target = "_blank";
 
         imageLink.appendChild(image);
-    })
+        searchResult.appendChild(imageLink);
+    });
+    showMore.style.display = "block";
 }
 
-searchForm.addEventListener("Submit", (e) =>{
+searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click", () => {
+    page++;
     searchImages();
 });
